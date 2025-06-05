4887fbdc472afafc9f1f9e9297ad7931c37b93cc
arinming <jyandysys1@naver.com>
2025-06-05 18:44:55 +0900

    [Feat] Kok App 네비게이션 설정

--- app/src/main/java/com/hgdgds/kokkok/MainActivity.kt
+++ app/src/main/java/com/hgdgds/kokkok/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.hgdgds.kokkok.ui.KokApp
+import com.hgdgds.kokkok.ui.rememberKokAppState
 import com.hgdgds.kokkok.ui.theme.KokKokTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,30 +22,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val appState = rememberKokAppState()
+
             KokKokTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                KokApp(appState)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KokKokTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/hgdgds/kokkok/ui/KokApp.kt
@@ -0,0 +1,201 @@
+package com.hgdgds.kokkok.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.NavigationRailItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.WindowAdaptiveInfo
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteDefaults
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteItemColors
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldDefaults
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTagsAsResourceId
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
+import com.hgdgds.kokkok.navigation.KokNavHost
+import kotlin.reflect.KClass
+
+@Composable
+fun KokApp(
+    appState: KokAppState,
+    modifier: Modifier = Modifier,
+    windowAdaptiveInfo: WindowAdaptiveInfo = currentWindowAdaptiveInfo(),
+) {
+    val currentDestination = appState.currentDestination
+
+    KokNavigationSuiteScaffold(
+        navigationSuiteItems = {
+            appState.topLevelDestinations.forEach { destination ->
+                val selected = currentDestination
+                    .isRouteInHierarchy(destination.baseRoute)
+                item(
+                    selected = selected,
+                    onClick = { appState.navigateToTopLevelDestination(destination) },
+                    icon = {
+                        Icon(
+                            imageVector = destination.unselectedIcon,
+                            contentDescription = null,
+                        )
+                    },
+                    selectedIcon = {
+                        Icon(
+                            imageVector = destination.selectedIcon,
+                            contentDescription = null,
+                        )
+                    },
+                    label = { Text(stringResource(destination.iconTextId)) },
+                )
+            }
+        },
+        windowAdaptiveInfo = windowAdaptiveInfo,
+    ) {
+        Scaffold(
+            modifier = modifier.semantics {
+                testTagsAsResourceId = true
+            },
+            containerColor = Color.Transparent,
+            contentColor = MaterialTheme.colorScheme.onBackground,
+            contentWindowInsets = WindowInsets(0, 0, 0, 0),
+        ) { padding ->
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+                    .consumeWindowInsets(padding)
+                    .windowInsetsPadding(
+                        WindowInsets.safeDrawing.only(
+                            WindowInsetsSides.Horizontal,
+                        ),
+                    ),
+            ) {
+                Box(
+                    modifier = Modifier.consumeWindowInsets(
+                        WindowInsets(0, 0, 0, 0)
+                    ),
+                ) {
+                    KokNavHost(
+                        appState = appState,
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun KokNavigationSuiteScaffold(
+    navigationSuiteItems: KokNavigationSuiteScope.() -> Unit,
+    modifier: Modifier = Modifier,
+    windowAdaptiveInfo: WindowAdaptiveInfo = currentWindowAdaptiveInfo(),
+    content: @Composable () -> Unit,
+) {
+    val layoutType = NavigationSuiteScaffoldDefaults
+        .calculateFromAdaptiveInfo(windowAdaptiveInfo)
+    val navigationSuiteItemColors = NavigationSuiteItemColors(
+        navigationBarItemColors = NavigationBarItemDefaults.colors(
+            selectedIconColor = KokNavigationDefaults.navigationSelectedItemColor(),
+            unselectedIconColor = KokNavigationDefaults.navigationContentColor(),
+            selectedTextColor = KokNavigationDefaults.navigationSelectedItemColor(),
+            unselectedTextColor = KokNavigationDefaults.navigationContentColor(),
+            indicatorColor = KokNavigationDefaults.navigationIndicatorColor(),
+        ),
+        navigationRailItemColors = NavigationRailItemDefaults.colors(
+            selectedIconColor = KokNavigationDefaults.navigationSelectedItemColor(),
+            unselectedIconColor = KokNavigationDefaults.navigationContentColor(),
+            selectedTextColor = KokNavigationDefaults.navigationSelectedItemColor(),
+            unselectedTextColor = KokNavigationDefaults.navigationContentColor(),
+            indicatorColor = KokNavigationDefaults.navigationIndicatorColor(),
+        ),
+        navigationDrawerItemColors = NavigationDrawerItemDefaults.colors(
+            selectedIconColor = KokNavigationDefaults.navigationSelectedItemColor(),
+            unselectedIconColor = KokNavigationDefaults.navigationContentColor(),
+            selectedTextColor = KokNavigationDefaults.navigationSelectedItemColor(),
+            unselectedTextColor = KokNavigationDefaults.navigationContentColor(),
+        ),
+    )
+
+    NavigationSuiteScaffold(
+        navigationSuiteItems = {
+            KokNavigationSuiteScope(
+                navigationSuiteScope = this,
+                navigationSuiteItemColors = navigationSuiteItemColors,
+            ).run(navigationSuiteItems)
+        },
+        layoutType = layoutType,
+        containerColor = Color.Transparent,
+        navigationSuiteColors = NavigationSuiteDefaults.colors(
+            navigationBarContentColor = KokNavigationDefaults.navigationContentColor(),
+            navigationRailContainerColor = Color.Transparent,
+        ),
+        modifier = modifier,
+    ) {
+        content()
+    }
+}
+
+
+class KokNavigationSuiteScope internal constructor(
+    private val navigationSuiteScope: NavigationSuiteScope,
+    private val navigationSuiteItemColors: NavigationSuiteItemColors,
+) {
+    fun item(
+        selected: Boolean,
+        onClick: () -> Unit,
+        modifier: Modifier = Modifier,
+        icon: @Composable () -> Unit,
+        selectedIcon: @Composable () -> Unit = icon,
+        label: @Composable (() -> Unit)? = null,
+    ) = navigationSuiteScope.item(
+        selected = selected,
+        onClick = onClick,
+        icon = {
+            if (selected) {
+                selectedIcon()
+            } else {
+                icon()
+            }
+        },
+        label = label,
+        colors = navigationSuiteItemColors,
+        modifier = modifier,
+    )
+}
+
+object KokNavigationDefaults {
+    @Composable
+    fun navigationContentColor() = MaterialTheme.colorScheme.onSurfaceVariant
+
+    @Composable
+    fun navigationSelectedItemColor() = MaterialTheme.colorScheme.onPrimaryContainer
+
+    @Composable
+    fun navigationIndicatorColor() = MaterialTheme.colorScheme.primaryContainer
+}
+
+
+private fun NavDestination?.isRouteInHierarchy(route: KClass<*>) =
+    this?.hierarchy?.any {
+        it.hasRoute(route)
+    } ?: false
